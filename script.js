c1f4cf8aec6160455f99ff0777af662e48a67110
Kamal Hussain <kmlhssn786@gmail.com>
2020-12-23 13:15:50 +0000

    Edit section added

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 todo = $(".txtb");
 
-$(".txtb").on("keyup", function(e){
+$(".txtb").on("keyup", function (e) {
 
     // 13 means enter button
-    if(e.keyCode ==13 && todo.val() != "")
-    {
+    if (e.keyCode == 13 && todo.val() != "") {
         var task = $('<div class="task"></div>').text($(".txtb").val());
-        var del = $('<i class="fas fa-trash-alt"></i>').click(function(){
+        var del = $('<i class="fas fa-trash-alt"></i>').click(function () {
             var p = $(this).parent();
-            p.fadeOut(function(){
+            p.fadeOut(function () {
                 p.remove();
             });
         });
-        var check = $('<i class="fas fa-check"></i>').click(function(){
+        var check = $('<i class="fas fa-check"></i>').click(function () {
             var p = $(this).parent();
-            p.fadeOut(function(){
+            p.fadeOut(function () {
                 $(".comp").append(p);
                 p.fadeIn();
             });
             $(this).remove();
         });
-        // var edit = $('<i class="fas fa-edit" ></i>').click(function(){
-        // var edit = $('<i class="fas fa-edit"></i>').click(function(){
-        //     var p = $(this).parent();
-        //     console.log(p);
-        //     p.attr('contenteditable', 'true').css("color", "red");
-        //     p.focus();
-            
-                
-        //     });
-
-        task.append(del, check);
+        var editIcon = $('<i class="fas fa-edit"></i>')
+        var edit = editIcon.click(function () {
+            var p = $(this).parent();
+            //editIcon.hide();
+            p.attr({ class: "task-edit",  contenteditable: true })
+            .focus(function () {
+                p.blur(function () {
+                    $(this).attr({class: "task", contenteditable: false }).css({ color: "", background: "" })
+                });
+            });
+        });
+        task.append(del, check, edit);
         $(".notcomp").append(task);
         //to clear the input value
         $(".txtb").val("");
-    }
+    };
 });
