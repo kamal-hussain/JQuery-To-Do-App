c2929bb11e59d6c987ad3f0446201e733e3e123e
Kamal Hussain <kmlhssn786@gmail.com>
2020-12-24 22:30:45 +0000

    Redo icon added

--- script.js
+++ script.js
@@ -11,24 +11,37 @@ $(".txtb").on("keyup", function (e) {
                 p.remove();
             });
         });
-        var check = $('<i class="fas fa-check"></i>').click(function () {
+
+        var check = $('<i class="fas fa-check"></i>').on('click', function () {
             var p = $(this).parent();
             p.fadeOut(function () {
                 $(".comp").append(p);
+                task.append(redo);
                 p.fadeIn();
             });
-            $(this).remove();
+            $(this).hide();
+            redo.show();
         });
+
+        var redo = $('<i class="fas fa-redo"></i>').click(function () {
+            var p = $(this).parent();
+            console.log(p);
+            p.fadeOut(function () {
+                $(".notcomp").append(p);
+                p.fadeIn();
+                check.show(); 
+            });
+            $(this).hide();
+        });
+        
         var editIcon = $('<i class="fas fa-edit"></i>')
         var edit = editIcon.click(function () {
             var p = $(this).parent();
-            //editIcon.hide();
-            p.attr({ class: "task-edit",  contenteditable: true })
-            .focus(function () {
+                p.attr({ class: "task-edit", contenteditable: true }).focus();
                 p.blur(function () {
-                    $(this).attr({class: "task", contenteditable: false }).css({ color: "", background: "" })
+                    $(this).attr({ class: "task", contenteditable: false })
+                    .css({ color: "", background: "" });
                 });
-            });
         });
         task.append(del, check, edit);
         $(".notcomp").append(task);
